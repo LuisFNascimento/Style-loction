da72fb07e0057db1ed139dff0f05cbf3fbd757c5
Luís Fernando Nascimento <105133507+LuisFNascimento@users.noreply.github.com>
2026-02-24 17:49:10 -0300

    first commit

---
+++ locacao/locacao-infra/src/test/java/br/com/style_loction/locacao/LocacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.style_loction.locacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
